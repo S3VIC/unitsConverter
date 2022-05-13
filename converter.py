0d824ca19f672dab306b9c0c00de52e850ac4c91
S3VIC <sewkipa@gmail.com>
2022-05-13 18:41:11 +0200

    Extending list of available units in menu, compressing dictionaries

--- converter.py
+++ converter.py
@@ -3,42 +3,29 @@ import PySimpleGUI as sg
 def convertDistance(units, value):
     #Defining distance units dictionary
     distanceUnitsDict = {
-    'pm':1E12,
-    'A' :1E10,
-    'nm':1E9,
-    'um':1E6,
-    'mm':1E3,
-    'dm':1E1,
-    'm':1,
-    'cm':1E2,
-    'km':1E-3
+    'pm':1E12,'A' :1E10, 'nm':1E9, 'um':1E6, 'mm':1E3, 'dm':1E1, 'm':1, 'cm':1E2, 'km':1E-3
     }
+
     #Table for 2 multipliers specified by given units in units
     multipliers = []
     #Getting those multipliers by their keys from defined dictionary
     multipliers.append(distanceUnitsDict[units[0]])
     multipliers.append(distanceUnitsDict[units[1]])
 
+    #returning the result of convertion
     return value * multipliers[1] / multipliers[0]
 
 def convertMass(units, value):
     massUnitsDict = {
-    'pg':1E12,
-    'ng':1E9,
-    'ug':1E6,
-    'mg':1E3,
-    'g':1,
-    'kg':1E-3,
-    't':1E-6
+    'pg':1E12, 'ng':1E9, 'ug':1E6, 'mg':1E3, 'g':1, 'kg':1E-3, 't':1E-6
     }
+    #Table for 2 multipliers specified by given units in units
     multipliers = []
-    for keys in massUnitsDict:
-        if keys == units[0]:
-            multipliers.append(massUnitsDict[keys])
+    #Getting those multipliers by their keys from defined dictionary
+    multipliers.append(massUnitsDict[units[0]])
+    multipliers.append(massUnitsDict[units[1]])
 
-        if keys == units[1]:
-            multipliers.append(massUnitsDict[keys])
-
+    #returning the result of convertion
     return value * multipliers[1] / multipliers[0]
 
 layout = [
@@ -46,10 +33,10 @@ layout = [
     sg.Text('Distance convertion')
     ],
     [
-    sg.Text('First value'),
-    sg.Spin(['m', 'g'], key = '-SPIN1'),
-    sg.Text('Second value'),
-    sg.Spin(['km', 'kg'], key = '-SPIN2-')
+    sg.Text('Convert from:'),
+    sg.Spin(['pm', 'A', 'nm', 'um', 'mm', 'cm', 'dm', 'm', 'km'], key = '-SPIN1'),
+    sg.Text('To:'),
+    sg.Spin(['pm', 'A', 'nm', 'um', 'mm', 'cm', 'dm', 'm', 'km'], key = '-SPIN2-')
     ],
 
     [sg.Button('Convert', key = '-BUTTON1-')],
